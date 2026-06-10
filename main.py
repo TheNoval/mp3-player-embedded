0963a58f786cdf33b769add72244805ca534670e
Nolan <nolan.oickle@wallawalla.edu>
2026-06-10 03:55:08 -0600

    Everything is basically working

--- main.py
+++ main.py
@@ -4,103 +4,131 @@ import encoder
 import player
 import songs
 
-MC = 0
-MS = 1
-mode = MC
+
+MODE_CONTROL = 0
+MODE_SONGS = 1
+
+PLAYING_DISPLAY_UPDATE_MS = 250
+
+mode = MODE_CONTROL
 display_dirty = True
+last_display_update = 0
 last_playing = False
-
-
-def render_display():
-    if mode == MC:
-        display.control(
-            songs.name(),
-            player.is_playing,
-            player.volume
-        )
-    else:
-        display.songs(
-            songs.song_list,
-            songs.display_current()
-        )
 
 
 def mark_display_dirty():
     global display_dirty
     display_dirty = True
 
+
+def play_selected():
+    player.play(songs.selected_file())
+    mark_display_dirty()
+
+
+def change_song(direction):
+    if direction > 0:
+        songs.next()
+    else:
+        songs.prev()
+    mark_display_dirty()
+
+
+def render_display():
+    if mode == MODE_CONTROL:
+        display.control(songs.name(), player.is_playing, player.volume)
+    else:
+        display.songs(songs.song_list, songs.display_current())
+
+
+def maybe_render():
+    global display_dirty, last_display_update
+
+    if not display_dirty:
+        return
+
+    if player.is_playing:
+        now = time.ticks_ms()
+        if time.ticks_diff(now, last_display_update) < PLAYING_DISPLAY_UPDATE_MS:
+            return
+        last_display_update = now
+
+    render_display()
+    display_dirty = False
+
+
+def handle_encoder():
+    d = encoder.read()
+    if d == 0:
+        return
+
+    if mode == MODE_CONTROL:
+        v = max(0, min(30, player.volume + d))
+        if v != player.volume:
+            player.volume = v
+            player.set_vol(v)
+            mark_display_dirty()
+    else:
+        change_song(d)
+
+
+def handle_clicks():
+    global mode
+
+    clicks = encoder.get_clicks()
+    if clicks == 3:
+        change_song(-1)
+        play_selected()
+    elif clicks == 2:
+        change_song(1)
+        play_selected()
+    elif clicks == 1:
+        if mode == MODE_CONTROL:
+            if player.is_playing:
+                player.pause()
+                mark_display_dirty()
+            else:
+                play_selected()
+        else:
+            mode = MODE_CONTROL
+            play_selected()
+
+
+def handle_mode_button():
+    global mode
+
+    if not encoder.mode_clicked():
+        return
+
+    if mode == MODE_CONTROL:
+        mode = MODE_SONGS
+    else:
+        mode = MODE_CONTROL
+    mark_display_dirty()
+
+
+def track_play_state():
+    global last_playing
+
+    if player.is_playing != last_playing:
+        last_playing = player.is_playing
+        mark_display_dirty()
+
+
 display.startup()
 songs.startup()
 time.sleep(2)
 
 while True:
-    if player.is_playing != last_playing:
-        last_playing = player.is_playing
-        mark_display_dirty()
+    track_play_state()
+    handle_encoder()
+    player.update(2)
 
-    if player.is_playing:
-        player.update(4)
-    else:
-        player.update(1)
+    handle_clicks()
+    handle_mode_button()
+    maybe_render()
 
-    d = encoder.read()
-
-    if d != 0:
-        if mode == MC:
-            v = player.volume + d
-            v = max(0, min(30, v))
-            if v != player.volume:
-                player.volume = v
-                player.set_vol(v)
-                mark_display_dirty()
-        else:
-            if d > 0:
-                songs.next()
-            else:
-                songs.prev()
-            mark_display_dirty()
-
-    clicks = encoder.get_clicks()
-
-    if clicks == 3:
-        songs.prev()
-        player.play(songs.selected_file())
-        mark_display_dirty()
-
-    elif clicks == 2:
-        songs.next()
-        player.play(songs.selected_file())
-        mark_display_dirty()
-
-    elif clicks == 1:
-        if mode == MC:
-            if player.is_playing:
-                player.pause()
-            else:
-                n = songs.selected_file()
-                player.play(n)
-            mark_display_dirty()
-        else:
-            mode = MC
-            n = songs.selected_file()
-            player.play(n)
-            mark_display_dirty()
-
-    if encoder.mode_clicked():
-        if mode == MC:
-            mode = MS
-        else:
-            mode = MC
-        mark_display_dirty()
-
-    if display_dirty:
-        render_display()
-        display_dirty = False
-
-    if player.is_playing:
-        player.update(4)
-    else:
-        player.update(1)
+    player.update(2)
 
     if not player.is_playing:
         time.sleep_ms(5)
--- player.py
+++ player.py
@@ -7,23 +7,45 @@ import sd_driver as sd
 from pins import I2S_SCK, I2S_WS, I2S_SD
 
 
+# Public playback state ------------------------------------------------------
+
 is_playing = False
 volume = 15
+
+
+# Audio/buffer tuning --------------------------------------------------------
 
 IBUF_SIZE = 49_152
 CHUNK_SIZE = 8192
 BUFFER_COUNT = 4
+GC_LOW_WATER = 8192
+
+
+# Stream state ---------------------------------------------------------------
 
 _filename = None
+_pending_filename = None
+_loading = False
 _file = None
 _i2s = None
+_info = None
 _bytes_left_to_read = 0
+
+
+# Ring buffer state ----------------------------------------------------------
 
 _buffers = [bytearray(CHUNK_SIZE) for _ in range(BUFFER_COUNT)]
 _views = [memoryview(buf) for buf in _buffers]
 _lengths = [0 for _ in range(BUFFER_COUNT)]
+
+_read_index = 0
 _write_index = 0
 _filled_count = 0
+_reserved_count = 0
+_stream_id = 0
+
+
+# Thread/debug state ---------------------------------------------------------
 
 _thread_started = False
 _lock = _thread.allocate_lock()
@@ -33,60 +55,116 @@ min_free = 999999
 memory_errors = 0
 
 
-def _i2s_format(channels):
-    if channels == 1:
-        return I2S.MONO
-    return I2S.STEREO
+# Public API -----------------------------------------------------------------
+
+def play(filename):
+    """Start a new file, or resume the current file if it is already open."""
+    global is_playing, _i2s, _pending_filename
+
+    _lock.acquire()
+    try:
+        _start_thread()
+
+        if filename == _filename and _file is not None:
+            if _i2s is None and _info is not None:
+                _i2s = _open_i2s(_info)
+            resume_current = True
+        else:
+            _pending_filename = filename
+            is_playing = False
+            resume_current = False
+    finally:
+        _lock.release()
+
+    if resume_current:
+        update(4)
+        _set_playing(True)
+        print("RESUME", filename)
 
 
-def _open_i2s(info):
-    return I2S(
-        0,
-        sck=Pin(I2S_SCK),
-        ws=Pin(I2S_WS),
-        sd=Pin(I2S_SD),
-        mode=I2S.TX,
-        bits=info["bits_per_sample"],
-        format=_i2s_format(info["channels"]),
-        rate=info["sample_rate"],
-        ibuf=IBUF_SIZE,
+def pause():
+    _set_playing(False)
+    print("PAUSE")
+    gc.collect()
+    stats()
+
+
+def stop():
+    global _pending_filename
+
+    _lock.acquire()
+    try:
+        _pending_filename = None
+        old_filename = _filename
+        _close_current()
+    finally:
+        _lock.release()
+
+    if old_filename is not None:
+        print("STOP", old_filename)
+    gc.collect()
+
+
+def update(max_buffers=2):
+    """Fill up to max_buffers empty audio buffers from the SD card."""
+    for _ in range(max_buffers):
+        if _pending_filename is not None or _loading:
+            break
+        if _reserved_count > 0:
+            break
+        if not _fill_one_buffer():
+            break
+
+
+def set_vol(vol):
+    global volume
+    volume = max(0, min(30, vol))
+
+
+def stats():
+    print(
+        "underruns:",
+        underruns,
+        "min_free:",
+        min_free,
+        "current_free:",
+        gc.mem_free(),
+        "mem_errors:",
+        memory_errors,
+        "filled:",
+        _filled_count,
+        "reserved:",
+        _reserved_count,
     )
 
 
-def _reset_buffers():
-    global _write_index, _filled_count
-    _write_index = 0
-    _filled_count = 0
-    for i in range(BUFFER_COUNT):
-        _lengths[i] = 0
+# Stream setup/teardown ------------------------------------------------------
+
+def _set_playing(value):
+    global is_playing
+    _lock.acquire()
+    try:
+        is_playing = value
+    finally:
+        _lock.release()
 
 
-def _close_current():
-    global is_playing, _filename, _file, _i2s, _bytes_left_to_read
-
-    is_playing = False
-    _bytes_left_to_read = 0
-    _reset_buffers()
-
-    if _i2s is not None:
-        _i2s.deinit()
-        _i2s = None
-
-    if _file is not None:
-        _file.close()
-        _file = None
-
-    _filename = None
+def _start_thread():
+    global _thread_started
+    if not _thread_started:
+        _thread.start_new_thread(_audio_thread, ())
+        _thread_started = True
 
 
 def _load(filename):
-    global _filename, _file, _i2s, _bytes_left_to_read
+    global _filename, _file, _i2s, _info, _bytes_left_to_read
     global underruns, min_free, memory_errors
 
     if filename is None:
         print("No song selected")
         return False
 
+    gc.collect()
     underruns = 0
     min_free = gc.mem_free()
     memory_errors = 0
@@ -117,44 +195,81 @@ def _load(filename):
     _file = sd.open_file(filename, "rb")
     _file.seek(info["data_offset"])
     _i2s = _open_i2s(info)
+    _info = info
     _bytes_left_to_read = info["data_size"]
     _filename = filename
     _reset_buffers()
     return True
 
 
-def _start_thread():
-    global _thread_started
-    if not _thread_started:
-        _thread.start_new_thread(_audio_thread, ())
-        _thread_started = True
+def _close_current():
+    global is_playing, _filename, _file, _i2s, _info, _bytes_left_to_read
+
+    is_playing = False
+    _bytes_left_to_read = 0
+    _reset_buffers()
+    _close_i2s()
+
+    if _file is not None:
+        _file.close()
+        _file = None
+
+    _filename = None
+    _info = None
+
+
+def _close_i2s():
+    global _i2s
+    if _i2s is not None:
+        _i2s.deinit()
+        _i2s = None
+
+
+def _open_i2s(info):
+    return I2S(
+        0,
+        sck=Pin(I2S_SCK),
+        ws=Pin(I2S_WS),
+        sd=Pin(I2S_SD),
+        mode=I2S.TX,
+        bits=info["bits_per_sample"],
+        format=_i2s_format(info["channels"]),
+        rate=info["sample_rate"],
+        ibuf=IBUF_SIZE,
+    )
+
+
+def _i2s_format(channels):
+    if channels == 1:
+        return I2S.MONO
+    return I2S.STEREO
+
+
+# Buffer producer ------------------------------------------------------------
+
+def _reset_buffers():
+    global _read_index, _write_index, _filled_count, _reserved_count, _stream_id
+
+    _stream_id += 1
+    _read_index = 0
+    _write_index = 0
+    _filled_count = 0
+    _reserved_count = 0
+
+    for i in range(BUFFER_COUNT):
+        _lengths[i] = 0
 
 
 def _fill_one_buffer():
-    global _bytes_left_to_read, _filled_count, min_free, memory_errors
+    global _bytes_left_to_read, _write_index
+    global _filled_count, _reserved_count, min_free, memory_errors
 
-    _lock.acquire()
-    try:
-        if _file is None or _bytes_left_to_read <= 0:
-            return False
-        if _filled_count >= BUFFER_COUNT:
-            return False
+    reserved = _reserve_write_slot()
+    if reserved is None:
+        return False
 
-        file_obj = _file
-        index = (_write_index + _filled_count) % BUFFER_COUNT
-        max_read = CHUNK_SIZE
-        if _bytes_left_to_read < max_read:
-            max_read = _bytes_left_to_read
-        if max_read == CHUNK_SIZE:
-            read_view = _views[index]
-        else:
-            read_view = _views[index][:max_read]
-    finally:
-        _lock.release()
-
-    free = gc.mem_free()
-    if free < min_free:
-        min_free = free
+    file_obj, stream_id, index, read_view, max_read = reserved
+    _collect_if_safe()
 
     try:
         n = file_obj.readinto(read_view)
@@ -166,78 +281,151 @@ def _fill_one_buffer():
     if n is None:
         n = max_read
 
+    return _commit_filled_buffer(file_obj, stream_id, index, n)
+
+
+def _reserve_write_slot():
+    global _write_index, _reserved_count
+
     _lock.acquire()
     try:
-        if file_obj is not _file:
+        if _file is None or _bytes_left_to_read <= 0:
+            return None
+        if _filled_count + _reserved_count >= BUFFER_COUNT:
+            return None
+
+        index = _write_index
+        _write_index = (_write_index + 1) % BUFFER_COUNT
+        _reserved_count += 1
+
+        max_read = CHUNK_SIZE
+        if _bytes_left_to_read < max_read:
+            max_read = _bytes_left_to_read
+
+        if max_read == CHUNK_SIZE:
+            read_view = _views[index]
+        else:
+            read_view = _views[index][:max_read]
+
+        return _file, _stream_id, index, read_view, max_read
+    finally:
+        _lock.release()
+
+
+def _commit_filled_buffer(file_obj, stream_id, index, length):
+    global _bytes_left_to_read, _filled_count, _reserved_count
+
+    _lock.acquire()
+    try:
+        if file_obj is not _file or stream_id != _stream_id:
+            _reserved_count -= 1
             return False
-        if n <= 0:
+        if length <= 0:
+            _reserved_count -= 1
             _bytes_left_to_read = 0
             return False
 
-        _lengths[index] = n
+        _lengths[index] = length
+        _reserved_count -= 1
         _filled_count += 1
-        _bytes_left_to_read -= n
+        _bytes_left_to_read -= length
         return True
     finally:
         _lock.release()
 
 
-def play(filename):
-    global is_playing
+def _collect_if_safe():
+    free = _track_free_memory()
+    if _filled_count >= 3 and free < GC_LOW_WATER:
+        gc.collect()
+        _track_free_memory()
+
+
+def _track_free_memory():
+    global min_free
+    free = gc.mem_free()
+    if free < min_free:
+        min_free = free
+    return free
+
+
+# Audio consumer thread ------------------------------------------------------
+
+def _audio_thread():
+    global is_playing, _pending_filename
+    global _read_index, _filled_count, underruns
+
+    while True:
+        pending, playing, i2s_obj, index, filled, bytes_left = _snapshot_state()
+
+        if pending is not None:
+            _handle_pending_file(pending)
+            continue
+
+        if not playing or i2s_obj is None:
+            time.sleep_ms(5)
+            continue
+
+        if filled <= 0:
+            if bytes_left <= 0:
+                stop()
+            else:
+                underruns += 1
+                time.sleep_ms(1)
+            continue
+
+        _write_all(i2s_obj, _views[index], _lengths[index])
+        _consume_buffer(i2s_obj)
+
+
+def _snapshot_state():
+    global _pending_filename
 
     _lock.acquire()
     try:
-        _start_thread()
-        same_file = filename == _filename and _file is not None
-        if same_file:
-            is_playing = True
-            print("RESUME", filename)
-            return
+        pending = _pending_filename
+        if pending is not None:
+            _pending_filename = None
 
-        _close_current()
-        loaded = _load(filename)
+        return (
+            pending,
+            is_playing,
+            _i2s,
+            _read_index,
+            _filled_count,
+            _bytes_left_to_read,
+        )
     finally:
         _lock.release()
 
-    if not loaded:
-        return
+
+def _handle_pending_file(filename):
+    global _loading
+
+    _lock.acquire()
+    try:
+        _loading = True
+        _close_current()
+    finally:
+        _lock.release()
 
     gc.collect()
-    update(4)
+
+    try:
+        loaded = _load(filename)
+    except Exception as e:
+        print("PLAY failed:", type(e), e)
+        loaded = False
+
+    if loaded:
+        update(4)
+        _set_playing(True)
 
     _lock.acquire()
     try:
-        is_playing = True
+        _loading = False
     finally:
         _lock.release()
-
-
-def pause():
-    global is_playing
-    _lock.acquire()
-    try:
-        is_playing = False
-    finally:
-        _lock.release()
-    print("PAUSE")
-    stats()
-
-
-def stop():
-    _lock.acquire()
-    try:
-        old_filename = _filename
-        _close_current()
-    finally:
-        _lock.release()
-    if old_filename is not None:
-        print("STOP", old_filename)
-
-
-def update(max_buffers=2):
-    for _ in range(max_buffers):
-        if not _fill_one_buffer():
-            break
 
 
 def _write_all(i2s_obj, view, length):
@@ -257,63 +445,14 @@ def _write_all(i2s_obj, view, length):
             written += count
 
 
-def _audio_thread():
-    global _write_index, _filled_count, underruns
+def _consume_buffer(i2s_obj):
+    global _read_index, _filled_count
 
-    while True:
-        _lock.acquire()
-        try:
-            playing = is_playing
-            i2s_obj = _i2s
-            index = _write_index
-            filled = _filled_count
-            bytes_left = _bytes_left_to_read
-
-            if playing and i2s_obj is not None and filled > 0:
-                view = _views[index]
-                length = _lengths[index]
-            else:
-                view = None
-                length = 0
-        finally:
-            _lock.release()
-
-        if not playing or i2s_obj is None:
-            time.sleep_ms(5)
-            continue
-
-        if filled <= 0:
-            if bytes_left <= 0:
-                stop()
-            else:
-                underruns += 1
-                time.sleep_ms(1)
-            continue
-
-        _write_all(i2s_obj, view, length)
-
-        _lock.acquire()
-        try:
-            if i2s_obj is _i2s and _filled_count > 0:
-                _lengths[_write_index] = 0
-                _write_index = (_write_index + 1) % BUFFER_COUNT
-                _filled_count -= 1
-        finally:
-            _lock.release()
-
-
-def set_vol(vol):
-    print("VOL", vol)
-
-
-def stats():
-    print(
-        "underruns:",
-        underruns,
-        "min_free:",
-        min_free,
-        "mem_errors:",
-        memory_errors,
-        "filled:",
-        _filled_count,
-    )
+    _lock.acquire()
+    try:
+        if i2s_obj is _i2s and _filled_count > 0:
+            _lengths[_read_index] = 0
+            _read_index = (_read_index + 1) % BUFFER_COUNT
+            _filled_count -= 1
+    finally:
+        _lock.release()
